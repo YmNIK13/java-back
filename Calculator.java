510fee6f28520ec31064b30ea5876f8fee06b63b
valery.stavitsky <valery.stavitsky@gmail.com>
2016-09-04 18:16:33 +0300

    user input

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+/**
+класс реализует калькулятор.
+*/
+public class Calculator {
+	/**
+	Результат вычисления.
+	*/
+	private int result;
+
+	/**
+	Суммируем аргументы.
+	@param params Аргументы суммирования.
+	*/
+	public void add(int ... params) {
+		for (Integer param : params) { 
+			this.result += param;
+		}
+	}
+
+	/**
+	получить результат.
+	@return результат вычисления.
+	*/
+	public int getResult() { 
+		return this.result;
+	}
+	
+	/**
+	Очистить результат вычисления.
+	*/
+	public void cleanResult() { 
+		this.result = 0;
+	}
+}
